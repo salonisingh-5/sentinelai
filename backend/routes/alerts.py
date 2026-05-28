a7dcce0b7488f552730d914b674d66156a307f6c
salonisingh-5 <singhsaloni1401@gmail.com>
2026-05-28 21:36:12 +0530

    Integrated AI classifier with alerts API

--- backend/routes/alerts.py
+++ backend/routes/alerts.py
@@ -2,7 +2,21 @@ from fastapi import APIRouter
 from database import SessionLocal
 from models import Alert
 
+# FIX AI IMPORT PATH
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../../")
+    )
+)
+
+# AI IMPORTS
+from ai.classifier import classify_log, get_fallback_explanation
+
 router = APIRouter()
+
 
 @router.get("/alerts")
 def get_alerts():
@@ -30,16 +44,32 @@ def create_alert(data: dict):
 
     db = SessionLocal()
 
+    # USER INPUT
+    log_text = data["log_text"]
+
+    # AI CLASSIFICATION
+    ai_result = classify_log(log_text)
+
+    threat_type = ai_result["threat_type"]
+    severity = ai_result["severity_label"]
+
+    # AI EXPLANATION
+    explanation_data = get_fallback_explanation(threat_type)
+    explanation = explanation_data["explanation"]
+
+    # SAVE TO DATABASE
     new_alert = Alert(
-        log_text=data["log_text"],
-        threat_type=data["threat_type"],
-        severity=data["severity"],
-        explanation=data["explanation"]
+        log_text=log_text,
+        threat_type=threat_type,
+        severity=severity,
+        explanation=explanation
     )
 
     db.add(new_alert)
     db.commit()
 
     return {
-        "message": "Alert created successfully"
+        "message": "AI alert created successfully",
+        "ai_result": ai_result,
+        "explanation": explanation_data
     }
